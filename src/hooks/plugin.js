0bad6e1a698e2a2d64e2963b58398aea7612fd38
Kananek Thongkam <info.dvgamer@gmail.com>
2025-12-23 16:12:52 +0700

    refactor: improve code formatting and consistency across multiple files

--- src/hooks/plugin.js
+++ src/hooks/plugin.js
@@ -1,248 +1,253 @@
 import { defineStore } from 'pinia';
 // Plugin hooks
 export const usePluginStore = defineStore('pluginStore', () => {
-    document.title = window.argv[3].plugin.uuid + ' - Plugin';
-    // Timer thread
-    const Timer = new Worker('interval.js');
-    const TimerSubscribe = [];
-    Timer.addEventListener('message', ({ data: { event, uuid } }) => {
-        const subIndex = TimerSubscribe.findIndex((item) => item.uuid === uuid);
-        subIndex !== -1 && event === 'setInterval' && TimerSubscribe[subIndex].fn();
-    });
-    // Create timer
-    const Interval = (uuid, delay, fn) => {
-        TimerSubscribe.findIndex((item) => item.uuid === uuid) === -1 && TimerSubscribe.push({ uuid, fn });
-        Timer.postMessage({ event: 'setInterval', uuid, delay });
-    };
-    // Destroy timer
-    const Unterval = (uuid) => {
-        const subIndex = TimerSubscribe.findIndex((item) => item.uuid === uuid);
-        subIndex !== -1 && TimerSubscribe.splice(subIndex, 1);
-        Timer.postMessage({ event: 'clearInterval', uuid });
-    };
-    // Connect to software
-    const message = ref();
-    const server = new WebSocket('ws://127.0.0.1:' + window.argv[0]);
-    server.onopen = () => server.send(JSON.stringify({ event: window.argv[2], uuid: window.argv[1] }));
-    server.onmessage = (e) => {
-        message.value = JSON.parse(e.data);
-        // console.log(e.data)
-    };
-    // Global settings data
-    const globalSettings = ref();
-    const devices = ref(new Set());
-    const userInfo = ref({});
-    // Set global settings data
-    const setGlobalSettings = (payload) => {
-        server.send(JSON.stringify({ event: 'setGlobalSettings', context: window.argv[1], payload }));
-        globalSettings.value = payload;
-    };
-    // Get global settings data
-    const getGlobalSettings = () => {
-        server.send(JSON.stringify({ event: 'getGlobalSettings', context: window.argv[1] }));
-    };
-    // Set background
-    const setBackground = (img, device, clearIcon = true) => {
-        server.send(JSON.stringify({
-            event: 'setBackground',
-            device: device,
-            payload: {
-                image: img,
-                clearIcon: clearIcon
-            }
-        }));
-    };
-    // Notify software that background stopped
-    const stopBackground = (device) => {
-        server.send(JSON.stringify({
-            event: 'stopBackground',
-            device: device,
-            payload: {
-                clearIcon: true // If clearIcon was set to true when setting background, this parameter needs to be true (tells software to restore icon)
-            }
-        }));
-    };
-    // Get user information
-    const getUserInfo = () => {
-        server.send(JSON.stringify({
-            event: 'getUserInfo'
-        }));
-    };
-    // Action data storage
-    class Actions {
-        settings;
-        action;
-        context;
-        title;
-        titleParameters = {};
-        constructor(action, context, settings) {
-            this.action = action;
-            this.context = context;
-            this.settings = settings;
+  document.title = window.argv[3].plugin.uuid + ' - Plugin';
+  // Timer thread
+  const Timer = new Worker('interval.js');
+  const TimerSubscribe = [];
+  Timer.addEventListener('message', ({ data: { event, uuid } }) => {
+    const subIndex = TimerSubscribe.findIndex((item) => item.uuid === uuid);
+    subIndex !== -1 && event === 'setInterval' && TimerSubscribe[subIndex].fn();
+  });
+  // Create timer
+  const Interval = (uuid, delay, fn) => {
+    TimerSubscribe.findIndex((item) => item.uuid === uuid) === -1 && TimerSubscribe.push({ uuid, fn });
+    Timer.postMessage({ event: 'setInterval', uuid, delay });
+  };
+  // Destroy timer
+  const Unterval = (uuid) => {
+    const subIndex = TimerSubscribe.findIndex((item) => item.uuid === uuid);
+    subIndex !== -1 && TimerSubscribe.splice(subIndex, 1);
+    Timer.postMessage({ event: 'clearInterval', uuid });
+  };
+  // Connect to software
+  const message = ref();
+  const server = new WebSocket('ws://127.0.0.1:' + window.argv[0]);
+  server.onopen = () => server.send(JSON.stringify({ event: window.argv[2], uuid: window.argv[1] }));
+  server.onmessage = (e) => {
+    message.value = JSON.parse(e.data);
+    // console.log(e.data)
+  };
+  // Global settings data
+  const globalSettings = ref();
+  const devices = ref(new Set());
+  const userInfo = ref({});
+  // Set global settings data
+  const setGlobalSettings = (payload) => {
+    server.send(JSON.stringify({ event: 'setGlobalSettings', context: window.argv[1], payload }));
+    globalSettings.value = payload;
+  };
+  // Get global settings data
+  const getGlobalSettings = () => {
+    server.send(JSON.stringify({ event: 'getGlobalSettings', context: window.argv[1] }));
+  };
+  // Set background
+  const setBackground = (img, device, clearIcon = true) => {
+    server.send(
+      JSON.stringify({
+        event: 'setBackground',
+        device: device,
+        payload: {
+          image: img,
+          clearIcon: clearIcon
         }
-        // Add action
-        static list = [];
-        static addAction = (action, context, settings) => {
-            const instance = new Actions(action, context, settings);
-            this.list.push(instance);
-            return instance;
-        };
-        // Delete action
-        static delAction = (context) => {
-            const i = this.list.findIndex((item) => item.context === context);
-            i !== -1 && this.list.splice(i, 1);
-        };
-        // Get action data
-        static getAction = (context) => {
-            return Actions.list.find((item) => item.context === context);
-        };
-        // Get all action data
-        static getActions = (action) => {
-            return this.list.filter((item) => item.action === action);
-        };
-        // Notify property inspector
-        sendToPropertyInspector = (payload) => {
-            server.send(JSON.stringify({ event: 'sendToPropertyInspector', action: this.action, context: this.context, payload }));
-        };
-        // Toggle state
-        setState = (state) => {
-            server.send(JSON.stringify({ event: 'setState', context: this.context, payload: { state } }));
-        };
-        // Set title
-        setTitle = (title) => {
-            server.send(JSON.stringify({ event: 'setTitle', context: this.context, payload: { title, target: 0 } }));
-        };
-        // Set image
-        setImage = (url) => {
-            if (url.includes('data:')) {
-                server.send(JSON.stringify({ event: 'setImage', context: this.context, payload: { target: 0, image: url } }));
-                return;
-            }
-            const image = new Image();
-            image.src = url;
-            image.onload = () => {
-                const canvas = document.createElement('canvas');
-                canvas.width = image.naturalWidth;
-                canvas.height = image.naturalHeight;
-                const ctx = canvas.getContext('2d');
-                ctx.drawImage(image, 0, 0);
-                server.send(JSON.stringify({ event: 'setImage', context: this.context, payload: { target: 0, image: canvas.toDataURL('image/png') } }));
-            };
-        };
-        // Set persistent data
-        setSettings = (payload) => {
-            this.settings = payload;
-            server.send(JSON.stringify({ event: 'setSettings', context: this.context, payload }));
-        };
-        // Open in default browser
-        openUrl = (url) => {
-            server.send(JSON.stringify({ event: 'openUrl', payload: { url } }));
-        };
-        // Register screensaver event (tell software to take exclusive control of screensaver, can maintain screensaver entry time independently)
-        registrationScreenSaverEvent = (device) => {
-            server.send(JSON.stringify({
-                event: 'registrationScreenSaverEvent',
-                device: device,
-                context: this.context
-            }));
-        };
+      })
+    );
+  };
+  // Notify software that background stopped
+  const stopBackground = (device) => {
+    server.send(
+      JSON.stringify({
+        event: 'stopBackground',
+        device: device,
+        payload: {
+          clearIcon: true // If clearIcon was set to true when setting background, this parameter needs to be true (tells software to restore icon)
+        }
+      })
+    );
+  };
+  // Get user information
+  const getUserInfo = () => {
+    server.send(
+      JSON.stringify({
+        event: 'getUserInfo'
+      })
+    );
+  };
+  // Action data storage
+  class Actions {
+    settings;
+    action;
+    context;
+    title;
+    titleParameters = {};
+    constructor(action, context, settings) {
+      this.action = action;
+      this.context = context;
+      this.settings = settings;
     }
-    class EventEmitter {
-        events;
-        constructor() {
-            this.events = {};
-        }
-        // Subscribe to event
-        subscribe(event, listener) {
-            if (!this.events[event]) {
-                this.events[event] = [];
-            }
-            this.events[event].push(listener);
-        }
-        // Unsubscribe from event
-        unsubscribe(event) {
-            if (!this.events[event])
-                return;
-            this.events[event] = null;
-        }
-        // Emit event
-        emit(event, data) {
-            if (!this.events[event])
-                return;
-            this.events[event].forEach((listener) => listener(data));
-        }
-    }
-    // Non-reactive event bus, for global use only
-    const eventEmitter = new EventEmitter();
-    return {
-        message,
-        globalSettings,
-        eventEmitter, // For logic use only, not for component reactivity
-        devices,
-        userInfo,
-        Interval,
-        Unterval,
-        setGlobalSettings,
-        getGlobalSettings,
-        setBackground,
-        stopBackground,
-        getUserInfo,
-        ActionArr: Actions.list,
-        getAction: Actions.getAction,
-        addAction: Actions.addAction,
-        delAction: Actions.delAction,
-        getActions: Actions.getActions
+    // Add action
+    static list = [];
+    static addAction = (action, context, settings) => {
+      const instance = new Actions(action, context, settings);
+      this.list.push(instance);
+      return instance;
     };
+    // Delete action
+    static delAction = (context) => {
+      const i = this.list.findIndex((item) => item.context === context);
+      i !== -1 && this.list.splice(i, 1);
+    };
+    // Get action data
+    static getAction = (context) => {
+      return Actions.list.find((item) => item.context === context);
+    };
+    // Get all action data
+    static getActions = (action) => {
+      return this.list.filter((item) => item.action === action);
+    };
+    // Notify property inspector
+    sendToPropertyInspector = (payload) => {
+      server.send(JSON.stringify({ event: 'sendToPropertyInspector', action: this.action, context: this.context, payload }));
+    };
+    // Toggle state
+    setState = (state) => {
+      server.send(JSON.stringify({ event: 'setState', context: this.context, payload: { state } }));
+    };
+    // Set title
+    setTitle = (title) => {
+      server.send(JSON.stringify({ event: 'setTitle', context: this.context, payload: { title, target: 0 } }));
+    };
+    // Set image
+    setImage = (url) => {
+      if (url.includes('data:')) {
+        server.send(JSON.stringify({ event: 'setImage', context: this.context, payload: { target: 0, image: url } }));
+        return;
+      }
+      const image = new Image();
+      image.src = url;
+      image.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = image.naturalWidth;
+        canvas.height = image.naturalHeight;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(image, 0, 0);
+        server.send(JSON.stringify({ event: 'setImage', context: this.context, payload: { target: 0, image: canvas.toDataURL('image/png') } }));
+      };
+    };
+    // Set persistent data
+    setSettings = (payload) => {
+      this.settings = payload;
+      server.send(JSON.stringify({ event: 'setSettings', context: this.context, payload }));
+    };
+    // Open in default browser
+    openUrl = (url) => {
+      server.send(JSON.stringify({ event: 'openUrl', payload: { url } }));
+    };
+    // Register screensaver event (tell software to take exclusive control of screensaver, can maintain screensaver entry time independently)
+    registrationScreenSaverEvent = (device) => {
+      server.send(
+        JSON.stringify({
+          event: 'registrationScreenSaverEvent',
+          device: device,
+          context: this.context
+        })
+      );
+    };
+  }
+  class EventEmitter {
+    events;
+    constructor() {
+      this.events = {};
+    }
+    // Subscribe to event
+    subscribe(event, listener) {
+      if (!this.events[event]) {
+        this.events[event] = [];
+      }
+      this.events[event].push(listener);
+    }
+    // Unsubscribe from event
+    unsubscribe(event) {
+      if (!this.events[event]) return;
+      this.events[event] = null;
+    }
+    // Emit event
+    emit(event, data) {
+      if (!this.events[event]) return;
+      this.events[event].forEach((listener) => listener(data));
+    }
+  }
+  // Non-reactive event bus, for global use only
+  const eventEmitter = new EventEmitter();
+  return {
+    message,
+    globalSettings,
+    eventEmitter, // For logic use only, not for component reactivity
+    devices,
+    userInfo,
+    Interval,
+    Unterval,
+    setGlobalSettings,
+    getGlobalSettings,
+    setBackground,
+    stopBackground,
+    getUserInfo,
+    ActionArr: Actions.list,
+    getAction: Actions.getAction,
+    addAction: Actions.addAction,
+    delAction: Actions.delAction,
+    getActions: Actions.getActions
+  };
 });
 export const useWatchEvent = (type, MessageEvents) => {
-    const plugin = usePluginStore();
-    if (type === 'plugin') {
-        return watch(() => plugin.message, () => {
-            if (!plugin.message)
-                return;
-            if (plugin.message.action)
-                return;
-            MessageEvents[plugin.message.event]?.(JSON.parse(JSON.stringify(plugin.message)));
-            if (plugin.message.event === 'didReceiveGlobalSettings') {
-                plugin.globalSettings = plugin.message.payload.settings;
-            }
-            else if (plugin.message.event === 'deviceDidConnect') {
-                // console.log('Device connected:', plugin.message);
-                plugin.devices.add(plugin.message.device);
-            }
-            else if (plugin.message.event === 'deviceDidDisconnect') {
-                // console.log('Device disconnected:', plugin.message);
-                plugin.devices.delete(plugin.message.device);
-            }
-            else if (plugin.message.event === 'sendUserInfo') {
-                plugin.userInfo = plugin.message.payload;
-            }
-        });
-    }
-    const Events = {
-        willAppear({ action, context, payload }) {
-            !plugin.getAction(context) && plugin.addAction(action, context, payload.settings);
-        },
-        willDisappear({ context }) {
-            plugin.delAction(context);
-        },
-        didReceiveSettings({ context, payload }) {
-            const action = plugin.getAction(context);
-            action.settings = payload.settings;
-        },
-        titleParametersDidChange({ context, payload }) {
-            const action = plugin.getAction(context);
-            action.title = payload.title;
-            action.titleParameters = payload.titleParameters;
+  const plugin = usePluginStore();
+  if (type === 'plugin') {
+    return watch(
+      () => plugin.message,
+      () => {
+        if (!plugin.message) return;
+        if (plugin.message.action) return;
+        MessageEvents[plugin.message.event]?.(JSON.parse(JSON.stringify(plugin.message)));
+        if (plugin.message.event === 'didReceiveGlobalSettings') {
+          plugin.globalSettings = plugin.message.payload.settings;
+        } else if (plugin.message.event === 'deviceDidConnect') {
+          // console.log('Device connected:', plugin.message);
+          plugin.devices.add(plugin.message.device);
+        } else if (plugin.message.event === 'deviceDidDisconnect') {
+          // console.log('Device disconnected:', plugin.message);
+          plugin.devices.delete(plugin.message.device);
+        } else if (plugin.message.event === 'sendUserInfo') {
+          plugin.userInfo = plugin.message.payload;
         }
-    };
-    return watch(() => plugin.message, () => {
-        if (!plugin.message)
-            return;
-        if (plugin.message.action !== MessageEvents['ActionID'])
-            return;
-        const data = JSON.parse(JSON.stringify(plugin.message));
-        Events[plugin.message.event]?.(data);
-        MessageEvents[plugin.message.event]?.(data);
-    });
+      }
+    );
+  }
+  const Events = {
+    willAppear({ action, context, payload }) {
+      !plugin.getAction(context) && plugin.addAction(action, context, payload.settings);
+    },
+    willDisappear({ context }) {
+      plugin.delAction(context);
+    },
+    didReceiveSettings({ context, payload }) {
+      const action = plugin.getAction(context);
+      action.settings = payload.settings;
+    },
+    titleParametersDidChange({ context, payload }) {
+      const action = plugin.getAction(context);
+      action.title = payload.title;
+      action.titleParameters = payload.titleParameters;
+    }
+  };
+  return watch(
+    () => plugin.message,
+    () => {
+      if (!plugin.message) return;
+      if (plugin.message.action !== MessageEvents['ActionID']) return;
+      const data = JSON.parse(JSON.stringify(plugin.message));
+      Events[plugin.message.event]?.(data);
+      MessageEvents[plugin.message.event]?.(data);
+    }
+  );
 };
--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,17 @@ import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'node:url';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            imports: ['vue'],
-            dts: './src/types/auto-imports.d.ts'
-        }),
-        viteSingleFile()
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue'],
+      dts: './src/types/auto-imports.d.ts'
+    }),
+    viteSingleFile()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  }
 });
